2f9de4c064c5c1e0ab70f7caee05ac93ee3d6910
JMerritt94 <Jay.P.Merritt@gmail.com>
2018-11-19 17:32:34 -0500

    turning in

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+let friends = [
+    {
+      name: "Kilee",
+     scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Disco Dave",
+     scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Dennis",
+     scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Kemba",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Lance",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Jason",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
